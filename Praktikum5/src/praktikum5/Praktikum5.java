df3f1826378fdc2a085e51bebeffdaa0424be6d9
Rafi Ramadhan <92287247+rafrmdhn@users.noreply.github.com>
2022-11-02 10:49:10 +0700

    Add files via upload

---
+++ Praktikum5/src/praktikum5/Praktikum5.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktikum5;
+
+/**
+ *
+ * @author Praktikan
+ */
+public class Praktikum5 {
+    public static void main(String[] sgra){
+        String firstname = "Rafi";
+        String lastname = "Ramadhan";
+        
+//        System.out.print(firstname);
+//        System.out.println(lastname);
+        String fullname = "Rafi" + " " + "Ramadhan";
+//        String fullname2 = firstname + " " + lastname;
+//        
+//        System.out.println(fullname);
+//        System.out.println(fullname2);
+//        String fullname = firstname + lastname;
+        
+//        String upper = fullname.toUpperCase();
+//        String lower = fullname.toLowerCase();
+//        
+//        System.out.println(upper);
+//        System.out.println(lower);
+        
+//        int panjang = fullname.length();
+//        
+//        System.out.println(panjang);
+//        System.out.println(fullname.substring(3,6);
+//        System.out.println(fullname.contains("Rafi"));
+//        System.out.println(fullname.equals("Rafi Ramadhan"));
+        
+//        int idx = fullname.indexOf("fi");
+//        
+//        System.out.println(idx);
+    }
+}
